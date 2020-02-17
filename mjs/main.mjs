1d3c0c8599d3a688e30bcb5cf04b1de361c6369e
Allen <e1668058@cmaisonneuve.qc.ca>
2020-02-17 14:26:36 -0500

    Animation : Ajout et appels des variables pour la classe d'animation

--- mjs/main.mjs
+++ mjs/main.mjs
@@ -3,10 +3,10 @@ import {anim_entete} from "./anim_entete.mjs";
 import { Slider } from "./Slider.mjs";
 import { config_slider } from "./config_slider.mjs";
 let elmEntete = document.querySelector("header .global");
-// let elm1 = document.getElementById("element1");
-// let elm2 = document.getElementById("bar");
-// const animTp = new anim_Tp(elm1, elm2); 
-// animTp.animation();
+let elm1 = document.getElementById("element1");
+let elm2 = document.getElementById("bar");
+const animTp = new anim_Tp(elm1, elm2); 
+animTp.animation();
 const animEntete = new anim_entete(elmEntete);
 animEntete.anim();
 const elmSlider = document.querySelector('.mon-slider')
